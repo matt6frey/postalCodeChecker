d0f8a4cc0e2dbb165686593c36c95840151bbc71
Matt Frey <matt6frey@gmail.com>
2020-01-03 11:31:38 -0700

    Added availability route. Working on functionality

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const xlsx = require('xlsx');
 const app = express();
 
 app.get('/ping', (request, response) => {
-	response.statusCode = 200;
-	response.json({
+    response.statusCode = 200;
+    response.json({
         message: "OK"
     });
 });
 
+
+app.get('/availability/:postalCode', (request, response) => {
+    const postalCode = (request.params.postalCode) ? request.params.postalCode : false;
+    if (postalCode) {
+        postalCode = postalCode.toUpperCase().trim();
+        if(postalCode.match('(\D\d\D\d\D\d|(\d{5})){1}').length) {
+            const locations = xlsx.readFile('db/postal_codes.xlsx');
+        }
+    }
+});
 
 app.listen(3000);
