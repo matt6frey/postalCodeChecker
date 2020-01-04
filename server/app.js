7fadf7360d4246d3cfb3f549c571437f2adc82ee
Matt Frey <matt6frey@gmail.com>
2020-01-03 17:51:53 -0700

    Prepared files for deployment

--- server/app.js
+++ server/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const xlsx = require('node-xlsx').default;
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+});
+
+app.get('/', (request, response) => {
+    response.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.get('/ping', (request, response) => {
@@ -22,7 +28,7 @@ app.get('/availability/:postalCode', (request, response) => {
         postalCode = postalCode.toUpperCase().trim();
         // Match Canadian or USA postal codes
         if(postalCode.match(/(\D\d\D\d\D\d){1}/) || postalCode.match(/\d{5}/) && postalCode.length === 5) {
-            const locations = xlsx.parse('db/postal_codes.xlsx')[0];
+            const locations = xlsx.parse(__dirname+'/db/postal_codes.xlsx')[0];
             results = locations.data.filter((row) => { 
                 if(row.indexOf(postalCode) > -1) {
                     return row;
